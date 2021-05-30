6147c038f87c7a327e9a58ea5d8c8b8711ed1123
Mohamed Mnete <mnetemohamed@gmail.com>
2021-05-30 14:44:24 -0500

    fixing extra fragment ui

--- app/src/main/java/com/example/dunyasesi/ui/main/WorldNetworkFragment.java
+++ app/src/main/java/com/example/dunyasesi/ui/main/WorldNetworkFragment.java
@@ -8,7 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.example.dunyasesi.R;
+import com.example.dunya_sesi.R;
 
 public class WorldNetworkFragment extends Fragment {
 
